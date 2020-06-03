683ba0d5134c4240c41b081cb5509fb6c4fbcf8b
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-03 17:52:26 -0400

    Create Steganography.py

---
+++ Steganography.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+#convert the image to binary in the form of a string
+def binconv (i):
+
+
+#encode the message within the image
+    def encode(image_name, raw_data):
+        #reads the image
+        read_image = cv2.imread(image_name)
+        
