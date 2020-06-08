7bb5a951f40aa236e0d0beb25f42d4325688e0b7
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-07 22:25:16 -0400

    Update Steganography.py

--- Steganography.py
+++ Steganography.py
@@ -1,12 +1,46 @@
-import cv2
-import numpy as np
+from stegano import lsbset
+from stegano.lsbset import generators
+import os
+from cryptography.fernet import Fernet
+#key for AES
 
-#convert the image to binary in the form of a string
-def binconv (i):
+#ask the user for the message that is encrypted
+message = input("Write your message: ")
 
+#encoding the message using AES
 
-#encode the message within the image
-    def encode(image_name, raw_data):
-        #reads the image
-        read_image = cv2.imread(image_name)
+#generating the key for AES
+def key_writing():
+    k = Fernet.generate_key()
+    with open("key.key","w+b") as file_k:
+        file_k.write(k)
         
+#Function that loads the key from the directory "key.key"
+def loading_key():   
+        return open("key.key", "r+b").read()
+
+#Encrypting the message into bytes
+key_writing()
+k = load_key()
+encrypted_message = message.encode()
+f = Fernet(key)
+encrypted = f.encrypt(encrypted_message)
+    
+#check what data type is being given and then converting to binary
+def convert_binary (message):
+    if isinstance(message, bytes):
+        return
+    elif isinstance(message, int):
+        return format(data, "08b")
+    else:
+        raise TypeError("Type is not supported.")
+
+#ask the user for the image that is used to hide the message
+image = input("Write the name of the image that is being used: ")
+
+#decrypt the AES message
+decrypted = f.decrypt(encrypted)
+#convert back to string
+original_message = decrypted.decode()
+
+
