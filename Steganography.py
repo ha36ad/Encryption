39c72075219c5af261481b1b57760a0666db31e5
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-08 22:23:27 -0400

    Update Steganography.py

--- Steganography.py
+++ Steganography.py
@@ -1,4 +1,4 @@
-from stegano import lsbset
+from stegano import lsb, lsbset
 from stegano.lsbset import generators
 import os
 from cryptography.fernet import Fernet
@@ -21,20 +21,11 @@ def loading_key():
 
 #Encrypting the message into bytes
 key_writing()
-k = load_key()
+k = loading_key()
 encrypted_message = message.encode()
-f = Fernet(key)
+f = Fernet(k)
 encrypted = f.encrypt(encrypted_message)
     
-#check what data type is being given and then converting to binary
-def convert_binary (message):
-    if isinstance(message, bytes):
-        return
-    elif isinstance(message, int):
-        return format(data, "08b")
-    else:
-        raise TypeError("Type is not supported.")
-
 #ask the user for the image that is used to hide the message
 image = input("Write the name of the image that is being used: ")
 
